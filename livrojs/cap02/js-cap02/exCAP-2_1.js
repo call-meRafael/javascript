00c66ef97ea2b5c6b31c876334cd6ba742dc81f4
countdown-br <raaphael.araujo@gmail.com>
2025-08-21 13:01:47 -0300

    atualizando os exercícios de javascript

---
+++ livrojs/cap02/js-cap02/exCAP-2_1.js
@@ -0,0 +1,12 @@
+// Cria referência ao formulário e ao subtítulo h3 (onde será exibida a reposta)
+
+const formulario = document.querySelector('form')
+const resposta = document.querySelector('h3')
+
+// Cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+
+formulario.addEventListener('submit', (e) => {
+    const nome = formulario.inNome.value  // obtem o nome digitado no formulario
+    resposta.innerText = `Olá, ${nome}!` // exibe a resposta do programa
+    e.preventDefault() // impede que o formulario seja submetido
+})
