0d7b22f7c346883b755db0fe6de4933d4e9f8ff8
countdown-br <raaphael.araujo@gmail.com>
2025-08-24 15:34:39 -0300

    general settings update

--- livrojs/cap02/js-cap02/testeCAP-2_1.js
+++ livrojs/cap02/js-cap02/testeCAP-2_1.js
@@ -8,14 +8,13 @@ const resposta2 = document.querySelector('h4')
 
 // Cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 formulario.addEventListener('submit', (e) => {
-    const titulo = formulario.inTitulo.value  // Obtém o conteúdo dos campos do formulário
-    const duracao = Number(formulario.inDuracao.value)  // Obtém o conteúdo dos campos do formulário
+    const titulo = formulario.inTitulo.value  // obtem o título do filme
+    const duracao = Number(formulario.inDuracao.value) // obtem a duração do filme em minutos
 
-    const horas = Math.floor(duracao / 60) // Areedonda para baixo (dividindo por 60) o resultado
-    const minutos = duracao % 60 // Obtém o resto da divisão por 60
+    const horas = Math.floor(duracao / 60)
+    const minutos = duracao % 60
 
-    resposta1.innerText = `O filme ${titulo} dura:`  // Exibe a resposta do programa
-    resposta2.innerText = `${horas} hora(s) e ${minutos} minutos`
-
-    e.preventDefault() // Impede que o formulário seja submetido
+    resposta1.innerText = titulo // exibe o título do filme
+    resposta2.innerText = `${horas} horas e ${minutos} minutos` // exibe a resposta do programa
+    e.preventDefault() // impede que o formulario seja submetido
 })
