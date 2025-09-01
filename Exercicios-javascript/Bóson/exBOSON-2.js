1228c410b0fa85b3d223beabee107a741f263ad3
countdown-br <raaphael.araujo@gmail.com>
2025-09-01 17:28:26 -0300

    atualizando exercício BOSON

---
+++ Exercicios-javascript/Bóson/exBOSON-2.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector('#frm1')
+const resultado1 = document.querySelector('#res1')
+const resultado2 = document.querySelector('#res2')
+
+// document.querySelector('#btnConfirmar').addEventListener('click', Confirmar)
+
+function Confirmar() {
+    
+    let nome = frm.inNome.value
+    let idade = Number(frm.inIdade.value)
+    let salario = Number(frm.inSalario.value)
+
+    resultado1.innerHTML = `Olá ${nome}, você tem ${idade} anos e uma remuneração de ${salario.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })}`
+}
