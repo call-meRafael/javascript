648bc1d791fc587666487900f257e50ac59f118a
countdown-br <raaphael.araujo@gmail.com>
2025-09-03 14:52:19 -0300

    atualizando exercicio sobre while  e for

---
+++ aula013/ambiente.js
@@ -0,0 +1,13 @@
+// console.log('Tudo bem?')
+
+// let contador = 1
+// while (contador <= 6) {
+//     console.log(`Passo ${contador}`)
+//     contador++
+// }
+
+let contador2 = 1
+do {
+    console.log(`Passo ${contador2}`)
+    contador2++
+} while (contador2 <= 10)
---
+++ aula013/novoAmbiente.js
@@ -0,0 +1,7 @@
+
+
+//-------------------------------
+console.log('Vai começar a contagem...')
+for (let c = 1; c <= 5; c++) {
+    console.log(c, 'Passo')
+}
